4846216e4435e288cdfd068e2f0d4990de6deed0
ArielRotolo <arielrotolo@frba.utn.edu.ar>
2024-04-16 04:58:46 +0000

    Se agrego la estructura kernel_config que guarda toda la configuración del modulo

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,44 +1,40 @@
 #include "main.h"
 
+int main(int argc, char* argv[]) {
 
-int main(int argc, char* argv[])
-{
-    
     t_log* logger = iniciar_logger();
-	t_config* config = iniciar_config();
-    
-    int puerto_escucha = config_get_int_value(config, "PUERTO_ESCUCHA");
-    char *ip_memoria = config_get_string_value(config,"IP_MEMORIA");
-    int puerto_memoria = config_get_int_value(config,"PUERTO_MEMORIA");
-    char *ip_cpu = config_get_string_value(config,"IP_CPU");
-    int puerto_cpu_dispatch = config_get_int_value(config,"PUERTO_CPU_DISPATCH");
-    int puerto_cpu_interrupt = config_get_int_value(config,"PUERTO_CPU_INTERRUPT");
-    char *algoritmo_planificacion = config_get_string_value(config,"ALGORITMO_PLANIFICACION");
-    int quantum = config_get_int_value(config,"QUANTUM");
-    char** recursos = config_get_array_value(config,"RECURSOS");
-    char** instancias_recursos = config_get_array_value(config,"INSTANCIAS_RECURSOS");
-    int grado_multiprogramacion = config_get_int_value(config,"GRADO_MULTIPROGRAMACION");
 
+    t_config* config = iniciar_config();
+
+    t_kernel_config* kernel_config = load_kernel_config(config);
+    
     //inicializar kernel
 
     //iniciar servidor de kernel
-	int fd_kernel = iniciar_servidor(puerto_escucha, logger, "KERNEL");
+	int fd_kernel = iniciar_servidor(kernel_config->puerto_escucha, logger, "KERNEL");
 	
 	//conectarme con cpu en modo dispatch
-	int fd_cpu_dispatch = crear_conexion(ip_cpu ,puerto_cpu_dispatch, logger, "CPU DISPATCH");
+	int fd_cpu_dispatch = crear_conexion(kernel_config->ip_cpu ,kernel_config->puerto_cpu_dispatch, logger, "CPU DISPATCH");
     log_info(logger, "[DISPATCH] CONECTADO a CPU");
 
     //conectarme con cpu en modo interrupt
-    int fd_cpu_interrupt = crear_conexion(ip_cpu ,puerto_cpu_interrupt, logger, "CPU INTERRUPT");
+    int fd_cpu_interrupt = crear_conexion(kernel_config->ip_cpu ,kernel_config->puerto_cpu_interrupt, logger, "CPU INTERRUPT");
     log_info(logger, "[INTERRUPT] CONECTADO a CPU");
 
 	//conectarme con memoria
-    int fd_memoria = crear_conexion(ip_memoria, puerto_memoria, logger, "MEMORIA");
+    int fd_memoria = crear_conexion(kernel_config->ip_memoria, kernel_config->puerto_memoria, logger, "MEMORIA");
     log_info(logger, "CONECTADO a MEMORIA");
 
     // esperar conexion de entradaSalida
     log_info(logger, "[KERNEL] esperando conexion de ENTRADA/SALIDA...");
     int fd_estradaSalida = esperar_cliente(fd_kernel, logger, "ENTRADA/SALIDA");
+
+    // Termino todo y libero los punteros usados
+
+    config_destroy(config); // Libera la memoria de config
+
+    free(kernel_config);
+
     return 0;
 }
 
@@ -47,17 +43,59 @@ t_config* iniciar_config(void) {
 	
     t_config* nuevo_config = config_create("kernel.config");
 	
-    if(nuevo_config == NULL) error_exit("Error, create config");
+    if(nuevo_config == NULL)
+        error_exit("Error al tratar de crear kernel.config");
 	
     return nuevo_config;
 }
 
 
 t_log* iniciar_logger(void) {
-	t_log* nuevo_logger = log_create("kernel.log","KERNEL",1,LOG_LEVEL_INFO);
-	if(nuevo_logger == NULL) error_exit("Error, create a new log.");
-	return nuevo_logger;
+	
+    t_log* nuevo_logger = log_create("kernel.log", "KERNEL", true, LOG_LEVEL_INFO);
+	
+    if(nuevo_logger == NULL)
+        error_exit("Error al tratar de crear kernel.log");
+	
+    return nuevo_logger;
 }
 
 
-void error_exit(char *message) { perror(message); exit(EXIT_FAILURE); }
+void error_exit(char *message) { 
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+t_kernel_config* load_kernel_config(t_config* config) { // Liberar kernel_config despues de usar
+
+    t_kernel_config* kernel_config = malloc(sizeof(t_kernel_config));
+    
+    if(kernel_config == NULL) {
+        perror("Fallo malloc");
+        exit(EXIT_FAILURE);
+    }
+    
+    kernel_config->puerto_escucha = config_get_int_value(config, "PUERTO_ESCUCHA");
+
+    kernel_config->ip_memoria = config_get_string_value(config,"IP_MEMORIA");
+
+    kernel_config->puerto_memoria = config_get_int_value(config,"PUERTO_MEMORIA");
+
+    kernel_config->ip_cpu = config_get_string_value(config,"IP_CPU");
+
+    kernel_config->puerto_cpu_dispatch = config_get_int_value(config,"PUERTO_CPU_DISPATCH");
+    
+    kernel_config->puerto_cpu_interrupt = config_get_int_value(config,"PUERTO_CPU_INTERRUPT");
+
+    kernel_config->algoritmo_planificacion = config_get_string_value(config,"ALGORITMO_PLANIFICACION");
+
+    kernel_config->quantum = config_get_int_value(config,"QUANTUM");
+
+    kernel_config->recursos = config_get_array_value(config,"RECURSOS");
+
+    kernel_config->instancias_recursos = config_get_array_value(config,"INSTANCIAS_RECURSOS");
+
+    kernel_config->grado_multiprogramacion = config_get_int_value(config,"GRADO_MULTIPROGRAMACION");
+
+    return kernel_config;
+}
--- kernel/src/main.h
+++ kernel/src/main.h
@@ -10,11 +10,30 @@
 #include <string.h>
 
 #include "server/server.h"
-
 #include "cliente/cliente.h"
 
+typedef char* String;
+
+typedef struct {
+    uint16_t puerto_escucha;
+    String ip_memoria;
+    uint16_t puerto_memoria;
+    String ip_cpu;
+    uint16_t puerto_cpu_dispatch;
+    uint16_t puerto_cpu_interrupt;
+    String algoritmo_planificacion;
+    uint16_t quantum;
+    String *recursos;
+    String *instancias_recursos;
+    uint16_t grado_multiprogramacion;
+} t_kernel_config;
+
 t_log* iniciar_logger(void);
+
 t_config* iniciar_config(void);
+
+t_kernel_config* load_kernel_config(t_config* config);
+
 void error_exit(char *);
 
 #endif
