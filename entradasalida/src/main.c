8ebd6e5eca9761ed124d005b017c86ac488f9366
LainoRamiro <rlaino@frba.utn.edu.ar>
2024-04-17 19:36:35 +0000

    Agrego cambios Interfaz

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
 
-    char* interfaz_name = "HOLA";
+    char* interfaz_name = "ACA VA EL NOMBRE DE LA INTERFAZ";
 
     int conexion_kernel = 0;
     int conexion_memoria = 0;
@@ -29,25 +29,25 @@ int main(int argc, char* argv[]) {
 
     switch(code){
         case STDIN:
-            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel);
-            conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
+            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel, logger, "KERNEL");
+            conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, logger, "MEMORIA");
 
             //IO_STDIN_READ
             //TEXT_STDIN
 
         case STDOUT:
-            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel);
-            conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
+            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel, logger, "KERNEL");
+            conexion_memoria = crear_conexion(ip_memoria, puerto_memoria), logger, "MEMORIA";
 
             //IO_STDOUT_WRITE
             //DIREC_READ
         case GENERIC:
-            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel);
+            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel, logger, "KERNEL");
 
             //IO_GEN_SLEEP
         case DIALFS:
-            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel);
-            conexion_memoria = crear_conexion(ip_memoria, puerto_memoria);
+            conexion_kernel = crear_conexion(ip_kernel , puerto_kernel, logger, "KERNEL");
+            conexion_memoria = crear_conexion(ip_memoria, puerto_memoria, logger, "MEMORIA");
 
             //IO_FS_CREATE
             //IO_FE_DELETE
@@ -86,6 +86,46 @@ t_log* iniciar_logger(void) {
 	if(nuevo_logger == NULL) error_exit("Error, create a new log.");
 	return nuevo_logger;
 }
+
+int crear_conexion(char *ip,  int puerto, t_log* logger, char* nombre_servidor)
+{
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(ip, string_itoa(puerto), &hints, &server_info);
+
+	// Ahora vamos a crear el socket.
+	//int socket_cliente = 0;
+	int socket_cliente = socket(server_info->ai_family,
+								server_info->ai_socktype,
+								server_info->ai_protocol);
+
+	// Ahora que tenemos el socket, vamos a conectarlo
+	
+    
+    int conexion = connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+    
+    if(conexion == -1 ) {
+        log_error(logger, "Error al conectarse a %s", nombre_servidor);
+        if (errno == ECONNREFUSED) {
+            log_info(logger, "El servidor %s rechazó la conexión\n", nombre_servidor);
+        } else if (errno == ETIMEDOUT) {
+            log_info(logger, "Se agotó el tiempo de espera al intentar conectarse a %s\n", nombre_servidor);
+        }
+        exit(EXIT_FAILURE);
+    }
+
+	log_info(logger, "Se conecto correctamente a %s", nombre_servidor);
+	freeaddrinfo(server_info);
+
+	return socket_cliente;
+}
+
 
 
 void error_exit(char *message) { perror(message); exit(EXIT_FAILURE); }
