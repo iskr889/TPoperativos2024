49b38211216d6962f18a2dde6bfbd5e195a62050
JoaquinMartinezUtn <joamartinez@frba.utn.edu.ar>
2024-04-17 20:09:51 -0300

    configuracion de server y client

--- cpu/src/main.h
+++ cpu/src/main.h
@@ -3,13 +3,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <readline/readline.h>
 #include <commons/log.h>
 #include <commons/string.h>
 #include <commons/config.h>
-#include <readline/readline.h>
-#include <utils/hello.h>
+#include <commons/collections/list.h>
+//#include <utils.h>
+//#include "server.h"
+
 
 t_log* iniciar_logger(void);
-t_config* iniciar_config(void);
+void terminar_programa(int, t_log*, t_config*);
+int iniciar_servidor(t_log*, char*, char*);
 void error_exit(char *);
+void iterator(char*);
+t_config* iniciar_config(void);
+void get_config_info(t_log*, t_config*);
+int crear_conexion(t_log*, char*, char*); 
+
+typedef struct {
+    char *ip_memoria;
+    char *puerto_memoria;
+    char *puerto_escucha_dispath;
+    char *puerto_escucha_interrupt;
+    char *cantidad_entradas;
+    char *algoritmo_tlb;
+} info_config_struct;
+
+info_config_struct info_config;
+
+//extern info_config_struct info_config;
 #endif
