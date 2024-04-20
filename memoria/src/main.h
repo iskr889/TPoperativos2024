85d2966aa32ede50d630c99cdc79d5727542581d
Ivan Saker <68342806+iskr889@users.noreply.github.com>
2024-04-20 15:56:19 +0000

    Update main.h

--- memoria/src/main.h
+++ memoria/src/main.h
@@ -9,14 +9,40 @@
 #include <readline/readline.h>
 #include <utils/hello.h>
 
+//Para el servidor
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<commons/collections/list.h>
+#include<string.h>
+#include<assert.h>
+
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+//config
 t_log* iniciar_logger(void);
 t_config* iniciar_config(void);
 void error_exit(char *);
-
 
 void crear_proceso(void);
 void terminar_proceso(void);
 void acceso_tabla_paginas(void);
 void ajustar_lenght_proceso(void);
 void acceso_espacio_usuario(void);
+
+//servidor memoria
+void inicio_server_memoria(void);
+void* recibir_buffer(int*, int);
+int iniciar_servidor(int puerto_escucha, t_log* logger);
+int esperar_cliente(int socket_servidor, t_log* logger);
+t_list* recibir_paquete(int);
+void recibir_mensaje(int);
+int recibir_operacion(int);
+void iterator(char* value);
+
 #endif
