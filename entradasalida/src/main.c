8ccd47aed71e882eb422a3ece030e64a50c8b8b8
leandro-quiroga <lequiroga@frba.utn.edu.ar>
2024-05-23 18:10:08 +0000

    agregando interfaz generica

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,53 +1,160 @@
 #include "main.h"
 
-// typedef enum {
-//     STDOUT,
-//     STDIN,
-//     GENERIC,
-//     DIALFS,
-//     ERROR
-// } interfaz_t ; 
-
 int main(int argc, char* argv[]) {
+    
+    char *ruta = "/home/utnso/Desktop/tp-2024-1c-groupSO2024/entradasalida/entradasalida.config"; //ruta de config
+    char *nombre = "GENERIC"; //nombre
+    
+    logger = iniciar_logger("entradaSalida.log", nombre, 1, LOG_LEVEL_INFO);
+    
+    char *t_interfaz = obtener_interfaz(ruta);
+    
+    // t_config* config = iniciar_config(ruta);
+    // char *interfaz = config_get_string_value(config, "TIPO_INTERFAZ");
+    // t_interfaz = malloc(strlen(interfaz));
+    // strcpy(t_interfaz, interfaz);
+    // config_destroy(config);
 
-    t_log* logger = iniciar_logger("entradasalida.log", "KERNEL", 1, LOG_LEVEL_INFO);
-    t_config* config = iniciar_config("entradasalida.config");
-    t_interfaz_config* interfaz_config = load_interfaz_config(config);
+    if(strcmp(t_interfaz, "GENERIC") == 0 ){
+        interfaz_generica(nombre, ruta);
 
-    log_info(logger, "Archivo de configuración cargado correctamente");
+    }else if(strcmp(t_interfaz, "STDIN") == 0){
+        interfaz_stdin(nombre, ruta);
 
-    // La interfaz intenta conectarse con la memoria
-    int conexion_memoria = conectarse_a_modulo("MEMORIA", interfaz_config->ip_memoria, interfaz_config->puerto_memoria, GENERIC_CON_MEMORIA, logger);
+    }else if(strcmp(t_interfaz, "STDOUT")== 0){
+        interfaz_stdout(nombre, ruta);
 
-    // La interfaz intenta conectarse con el kernel
-    int conexion_kernel = conectarse_a_modulo("KERNEL", interfaz_config->ip_kernel, interfaz_config->puerto_kernel, GENERIC_CON_KERNEL, logger);
+    }else if(strcmp(t_interfaz, "DialFS") == 0){
+        interfaz_dialFS(nombre, ruta);
 
-    close(conexion_kernel);
-    close(conexion_memoria);
+    }else {
+        log_error(logger, "Tipo de interfaz incorrecto!");
+        return -1;
+    }
+   
+    // // terminar programa
+    // close(conexion_kernel);
+    // close(conexion_memoria);
+    // log_destroy(logger);
+    // free(interfaz_config);
     log_destroy(logger);
-    config_destroy(config);
-    free(interfaz_config);
 
     return OK;
 }
 
-t_interfaz_config* load_interfaz_config(t_config* config){
+char *obtener_interfaz(char* ruta){
+    
+    // creo la interfaz
+    t_config* config = iniciar_config(ruta);
+    
+    char *interfaz = config_get_string_value(config, "TIPO_INTERFAZ");
+    
+    char *t_interfaz = string_duplicate(interfaz);
+    
+    config_destroy(config);
+    
+    return t_interfaz;
+}
+
+t_interfaz_config* crear_interfaz_config(){
+    
     t_interfaz_config* interfaz_config = malloc(sizeof(t_interfaz_config));
 
     if(interfaz_config == NULL) {
         perror("Fallo malloc");
         exit(EXIT_FAILURE);
     }
-
-    interfaz_config->tipo_interfaz = config_get_string_value(config, "TIPO_INTERFAZ");
-    interfaz_config->tiempo_u_trabajo = config_get_int_value(config, "TIEMPO_UNIDAD_TRABAJO");
-    interfaz_config->ip_kernel = config_get_string_value(config, "IP_KERNEL");
-    interfaz_config->puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
-    interfaz_config->ip_memoria = config_get_string_value(config, "IP_MEMORIA");
-    interfaz_config->puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
-    interfaz_config->path_base_dialfs = config_get_string_value(config, "PATH_BASE_DIALFS");
-    interfaz_config->block_size = config_get_int_value(config, "BLOCK_SIZE");
-    interfaz_config->block_count = config_get_int_value(config, "BLOCK_COUNT");
-
+    
     return interfaz_config;
 }
+
+
+t_interfaz_config* load_generic_config(t_config *config){
+
+    t_interfaz_config* interfaz_generica = crear_interfaz_config();
+
+    interfaz_generica->tipo_interfaz = config_get_string_value(config, "TIPO_INTERFAZ");
+    interfaz_generica->tiempo_u_trabajo = config_get_int_value(config, "TIEMPO_UNIDAD_TRABAJO");
+    interfaz_generica->ip_kernel = config_get_string_value(config, "IP_KERNEL");
+    interfaz_generica->puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
+
+    return interfaz_generica;
+}
+
+void io_gen_sleep(int unidades_trabajo){
+    if(unidades_trabajo < 0){
+        log_error(logger, "Unidades de trabajo Negativas!");
+        exit(EXIT_FAILURE);
+    }
+    sleep(unidades_trabajo);
+}
+
+char *recibir_instruccion(int fd){
+    //instruccion
+    void *instruccion = NULL;
+	//tamanio de la instruccion (string.length() )
+    int length;
+	if(recv(fd, &length, sizeof(int), MSG_WAITALL) > 0){ //MSG_WAIALL ESPERA QUE LLEGE TODO EL "PAQUETE"
+		instruccion = malloc(length + 1); // +1 por el caracter nulo
+        recv(fd, instruccion, length, MSG_WAITALL);
+        
+        return (char*) instruccion;
+	
+    }else{
+	    log_error(logger, "No se pudo recibir la instruccion");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int obtener_unidades_trabajo(int fd){
+    int u_trabajo;
+	if(recv(fd, &u_trabajo, sizeof(int), MSG_WAITALL) > 0) //MSG_WAIALL ESPERA QUE LLEGE TODO EL "PAQUETE"
+		return u_trabajo;
+	else
+	{
+		return -1;
+	} 
+}
+
+void interfaz_generica(char* nombre, char *ruta){
+    
+    t_config *config = iniciar_config(ruta);
+    
+    t_interfaz_config *generic_config = load_generic_config(config);
+
+    int conexion_kernel = conectarse_a_modulo("KERNEL", generic_config->ip_kernel, generic_config->puerto_kernel, GENERIC_CON_KERNEL, logger);
+
+    while(1){
+        
+        char *instruccion = recibir_instruccion(conexion_kernel);
+        
+        if(strcmp(instruccion, "IO_GEN_SLEEP") == 0){
+            
+            int u_trabajo = obtener_unidades_trabajo(conexion_kernel);
+            
+            if(u_trabajo <= -1){
+                log_error(logger, "No fue posible obtener las unidades de trabajo");
+                exit(EXIT_FAILURE);
+            }
+
+            io_gen_sleep(u_trabajo);
+            log_info(logger, "Instruccion completada con exito!");
+
+            // libero
+            close(conexion_kernel);
+            config_destroy(config);
+            free(generic_config);
+
+            exit(EXIT_SUCCESS);
+        
+        }else{
+            log_error(logger, "La instruccion recibida no es valida!");
+
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void interfaz_stdin(char *nombre, char *ruta){}
+void interfaz_stdout(char *nombre, char *ruta){}
+void interfaz_dialFS(char *nombre, char *ruta){}
--- entradasalida/src/main.h
+++ entradasalida/src/main.h
@@ -4,6 +4,7 @@
 #include "../../utils/src/cliente.h"
 #include "../../utils/src/utils.h"
 
+t_log* logger;
 typedef struct{
     String tipo_interfaz;
     uint64_t tiempo_u_trabajo;
@@ -16,10 +17,28 @@ typedef struct{
     uint64_t block_count;
 } t_interfaz_config;
 
-/**
-* @fn    Carga la configuración en la estructura interfaz_config
-* @brief Con el archivo config abierto solicita memoria y lo carga en la estructura interfaz_config
-*/
-t_interfaz_config* load_interfaz_config(t_config* config);
+// /**
+// * @fn    Carga la configuración en la estructura interfaz_config
+// * @brief Con el archivo config abierto solicita memoria y lo carga en la estructura interfaz_config
+// */
+// t_interfaz_config* load_interfaz_config(t_config* config);
 
+/*** CONFIG INTERFAZ ***/
+t_interfaz_config *load_generic_config(t_config *config);
+
+/*** INSTRUCCIONES **/
+void io_gen_sleep(int unidades_trabajo);
+
+/*** FUNCIONES AUXILIARES ***/
+char *recibir_instruccion(int fd);
+int obtener_unidades_trabajo(int fd);
+t_interfaz_config *crear_interfaz_config();
+char *obtener_interfaz(char* ruta);
+
+/*** INTERFACES **/
+void interfaz_generica(char* nombre, char *ruta);
+//terminar
+void interfaz_stdin(char *nombre, char *ruta);
+void interfaz_stdout(char *nombre, char *ruta);
+void interfaz_dialFS(char *nombre, char *ruta);
 #endif
