7ff6208492d8022364d1a5d11ac82f5faa4c848d
ArielRotolo <arielrotolo@frba.utn.edu.ar>
2024-06-04 02:06:23 +0000

    Paginación basica implementada en la memoria

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include "memory_interface_handler.h"
 
+void* user_memory; // Espacio contiguo de memoria
+t_bitarray* frame_bitarray;  // Bitarray para seguir el estado de los marcos
+
 t_config* config;
 t_memoria_config* memoria_config;
 t_log* logger;
@@ -78,4 +81,150 @@ t_memoria_config* load_memoria_config(String path) {
     memoria_config->retardo_respuesta = config_get_string_value(config,"RETARDO_RESPUESTA");
 
     return memoria_config;
+}
+
+void inicializar_memoria() {
+
+    user_memory = malloc(MEMORY_SIZE);
+
+    if(user_memory == NULL) {
+        perror("Error en malloc()");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t bitmap_size = (MEMORY_SIZE / PAGE_SIZE) / CHAR_BIT;
+
+    char* bitarray_data = calloc(bitmap_size, sizeof(char));
+
+    if(bitarray_data == NULL) {
+        perror("Error en calloc()");
+        exit(EXIT_FAILURE);
+    }
+
+    frame_bitarray = bitarray_create_with_mode(bitarray_data, bitmap_size, LSB_FIRST);
+}
+
+// Crear un proceso en memoria
+Proceso_t* crear_proceso(int pid, int cant_paginas) {
+
+    Proceso_t* proceso = malloc(sizeof(Proceso_t));
+    proceso->pid = pid;
+    proceso->cant_paginas = cant_paginas;
+    proceso->pagina = malloc(cant_paginas * sizeof(Page_table_t));
+
+    if(proceso->pagina == NULL) {
+        perror("Error en malloc()");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < cant_paginas; i++) {
+        proceso->pagina[i].marco = -1; // Al crearse el proceso no tiene marcos asignados a la memoria
+        proceso->pagina[i].asignada = false;
+    }
+
+    return proceso;
+}
+
+// Asignar páginas a un proceso
+int asignar_pagina(Proceso_t* proceso) {
+
+    int paginas_asignadas = 0;
+
+    for (int i = 0; i < bitarray_get_max_bit(frame_bitarray); i++) {
+
+        if (bitarray_test_bit(frame_bitarray, i) == false) {
+
+            proceso->pagina[paginas_asignadas].marco = i;
+            proceso->pagina[paginas_asignadas].asignada = true;
+            bitarray_set_bit(frame_bitarray, i);
+            paginas_asignadas++;
+
+            if (paginas_asignadas == proceso->cant_paginas)
+                return OK;
+        }
+    }
+
+    return ERROR;
+}
+
+// Liberar páginas de un proceso
+void liberar_paginas_proceso(Proceso_t* proceso) {
+
+    for (int i = 0; i < proceso->cant_paginas; i++) {
+        if (proceso->pagina[i].asignada)
+            bitarray_clean_bit(frame_bitarray, proceso->pagina[i].marco);
+    }
+
+    free(proceso->pagina);
+    free(proceso);
+}
+
+// Acceso a la tabla de páginas
+int acceder_marco(Proceso_t* proceso, int numero_de_pagina) {
+
+    if (numero_de_pagina >= proceso->cant_paginas)
+        return ERROR;
+
+    return proceso->pagina[numero_de_pagina].marco;
+}
+
+// Lectura desde el espacio de usuario
+int leer_memoria(int direccion_fisica, void* buffer, size_t size) {
+
+    if (direccion_fisica + size > MEMORY_SIZE) {
+        fprintf(stderr, "Lectura fuera de limites!\n");
+        return ERROR;
+    }
+
+    memcpy(buffer, user_memory + direccion_fisica, size);
+    return OK;
+}
+
+// Escritura en el espacio de usuario
+int escribir_memoria(int direccion_fisica, void* data, size_t size) {
+
+    if (direccion_fisica + size > MEMORY_SIZE) {
+        fprintf(stderr, "Escritura fuera de limites\n");
+        return ERROR;
+    }
+
+    memcpy(user_memory + direccion_fisica, data, size);
+    return OK;
+}
+
+// Ajustar tamaño de un proceso
+int resize_proceso(Proceso_t* proceso, int new_cant_paginas) {
+
+    if (new_cant_paginas > proceso->cant_paginas) { // Ampliación del proceso
+
+        int paginas_adicionales = new_cant_paginas - proceso->cant_paginas;
+
+        for (int i = 0; i < bitarray_get_max_bit(frame_bitarray) && paginas_adicionales > 0; i++) {
+
+            if (!bitarray_test_bit(frame_bitarray, i)) {
+
+                proceso->pagina = realloc(proceso->pagina, new_cant_paginas * sizeof(Page_table_t));
+                proceso->pagina[proceso->cant_paginas].marco = i;
+                proceso->pagina[proceso->cant_paginas].asignada = true;
+                bitarray_set_bit(frame_bitarray, i);
+                proceso->cant_paginas++;
+                paginas_adicionales--;
+            }
+        }
+
+        return (paginas_adicionales == 0) ? OK : ERROR; // ERROR: sin más memoria
+    }
+    
+    if (new_cant_paginas < proceso->cant_paginas) { // Reducción del proceso
+
+        for (int i = proceso->cant_paginas - 1; i >= new_cant_paginas; i--) {
+            if (proceso->pagina[i].asignada)
+                bitarray_clean_bit(frame_bitarray, proceso->pagina[i].marco);
+        }
+
+        proceso->pagina = realloc(proceso->pagina, new_cant_paginas * sizeof(Page_table_t));
+        proceso->cant_paginas = new_cant_paginas;
+    }
+
+    return OK;
 }
--- memoria/src/main.h
+++ memoria/src/main.h
@@ -3,6 +3,22 @@
 
 #include "../../utils/src/servidor.h"
 #include "../../utils/src/utils.h"
+#include <commons/bitarray.h>
+
+#define PAGE_SIZE 128 // Tamaño de pagina
+#define CANT_PAGINAS 128 // Cantidad de paginas
+#define MEMORY_SIZE (PAGE_SIZE * CANT_PAGINAS)  // Tamaño total de memoria
+
+typedef struct {
+    int marco;
+    bool asignada;
+} Page_table_t; // Tabla donde el indice de pagina representa el marco y si está asignado o no
+
+typedef struct {
+    int pid;
+    int cant_paginas;
+    Page_table_t* pagina;
+} Proceso_t;
 
 typedef struct {
     String puerto_escucha;
@@ -23,5 +39,21 @@ void liberar_memoria();
 * @brief Abre un archivo config en path y lo carga en la estructura memoria_config
 */
 t_memoria_config* load_memoria_config(String path);
+
+void inicializar_memoria();
+
+Proceso_t* crear_proceso(int pid, int cant_paginas);
+
+int asignar_pagina(Proceso_t* proceso);
+
+void liberar_paginas_proceso(Proceso_t* proceso);
+
+int acceder_marco(Proceso_t* proceso, int numero_de_pagina);
+
+int leer_memoria(int physical_address, void* buffer, size_t size);
+
+int escribir_memoria(int physical_address, void* data, size_t size);
+
+int resize_proceso(Proceso_t* proceso, int new_cant_paginas);
 
 #endif
