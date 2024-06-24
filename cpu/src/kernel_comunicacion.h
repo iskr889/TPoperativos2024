b6f745ed1afd2012f694d2987eef2e4c077eee6c
JoaquinMartinezUtn <joamartinez@frba.utn.edu.ar>
2024-06-23 23:53:17 -0300

    deleting unused files

--- cpu/src/kernel_comunicacion.h
+++
@@ -1,9 +0,0 @@
-#ifndef KERNEL_COMUNICACION_H_
-#define KERNEL_COMUNICACION_H_
-
-#include "main.h"
-
-void enviar_io_gen_sleep(int socket, uint32_t tiempo);
-uint32_t recibir_io_gen_sleep(int socket);
-
-#endif
